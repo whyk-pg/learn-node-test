58e191e30bbc8c312bbedc8d82999e4085d5844a
WhyK <windchimeyk@gmail.com>
2024-03-24 11:13:59 +0900

    feat(read-file): add fs/promises module

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import { program } from "commander";
+import * as fs from "node:fs/promises";
 
 program.parse(process.argv);
-
 const filePath = program.args[0];
-console.log(filePath);
+
+fs.readFile(filePath, { encoding: "utf8" })
+  .then((file) => {
+    console.log(file);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
